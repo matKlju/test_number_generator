bcd2df585384f75b0293a318b281ffd058ccf550
matKlju <108662553+matKlju@users.noreply.github.com>
2025-04-08 22:42:21 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+// server.js (repo2)
+const express = require('express');
+const app = express();
+
+app.get('/number', (req, res) => {
+  const num = Math.floor(Math.random() * 100000);
+  res.send(num.toString());
+});
+
+app.listen(5000, () => console.log('Number generator running on http://localhost:5000'));
